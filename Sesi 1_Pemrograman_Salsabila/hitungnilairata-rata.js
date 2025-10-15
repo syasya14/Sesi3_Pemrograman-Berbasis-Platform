4ecbed2eff42f52d1c63b06afdc3faa4d917a5f3
syasya14 <sdevina790@gmail.com>
2025-10-15 19:44:07 +0700

    Add files via upload

---
+++ Sesi 1_Pemrograman_Salsabila/hitungnilairata-rata.js
@@ -0,0 +1,16 @@
+let matematika = 85;
+let BahasaInggris = 90;
+let ipa = 85;
+
+let NilaiRataRata = (matematika + BahasaInggris + Ipa) / 3;
+
+console.log("Nilai matematika :" + matematika);
+console.log("Nilai Bahasa inggris :" + BahasaInggris);
+console.log("Nilai Ipa :" + Ipa);
+console.log("Nilai Rata Rata :" + NilaiRataRata);
+
+if (NilaiRataRata >=80) {
+    console.log("Selamat Anda lulus!")
+} else {
+    console.log("Maaf anda tidak Lulus")
+}
